32531718d48ac1a4bbf0de89650658d68fe812d8
Runeviperz <battlefield200@gmail.com>
2015-07-25 18:26:08 +1000

    -added a stack of achievements in userLocalStore

--- app/src/main/java/runeviperz/com/myapplication/UserLocalStore.java
+++ app/src/main/java/runeviperz/com/myapplication/UserLocalStore.java
@@ -12,6 +12,15 @@ public class UserLocalStore {
         userLocalDatabase = context.getSharedPreferences(SP_NAME, 0);
     }
 
+    /**********************************************************************************************
+
+
+                                            USER DATA
+
+
+     *********************************************************************************************/
+
+
     public void storeUserData(User user) {
         SharedPreferences.Editor spEditor = userLocalDatabase.edit();
         spEditor.putString("name", user.name);
@@ -65,6 +74,15 @@ public class UserLocalStore {
         spEditor.clear();
         spEditor.commit();
     }
+
+
+    /**********************************************************************************************
+
+
+                                        SCORES
+
+
+     *********************************************************************************************/
 
     public void setHighScore(int score) {
         SharedPreferences.Editor spEditor = userLocalDatabase.edit();
@@ -138,5 +156,158 @@ public class UserLocalStore {
         spEditor.commit();
     }
 
-}
+    /**********************************************************************************************
 
+
+                                            ACHIEVEMENTS
+
+
+     *********************************************************************************************/
+
+    public void setWon10Games() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("won10Games", true);
+        spEditor.commit();
+    }
+
+    public void setWon20Games() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("won20Games", true);
+        spEditor.commit();
+    }
+
+    public void setWon50Games() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("won50Games", true);
+        spEditor.commit();
+    }
+
+    public void setWon100Games() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("won100Games", true);
+        spEditor.commit();
+    }
+
+    public void setWon250Games() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("won250Games", true);
+        spEditor.commit();
+    }
+
+    public void setWonInExactly69() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("wonInExactly69", true);
+        spEditor.commit();
+    }
+
+    public void setWonInExactly100() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("wonInExactly100", true);
+        spEditor.commit();
+    }
+
+    public void setWonInExactly420() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("wonInExactly420", true);
+        spEditor.commit();
+    }
+
+    public void setWonInOver500() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("wonInOver500", true);
+        spEditor.commit();
+    }
+
+    public void setWonInOver700() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("wonInOver700", true);
+        spEditor.commit();
+    }
+
+    public void setWonInOver800() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("wonInOver800", true);
+        spEditor.commit();
+    }
+
+    public void setWonInOver900() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("wonInOver900", true);
+        spEditor.commit();
+    }
+
+    public void setWonInOver1000() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("wonInOver1000", true);
+        spEditor.commit();
+    }
+
+    public void setWonInOver9000() {
+        SharedPreferences.Editor spEditor = userLocalDatabase.edit();
+        spEditor.putBoolean("wonInOver9000", true);
+        spEditor.commit();
+    }
+
+
+    /*
+    GET METHODS
+     */
+
+    public boolean getWon10Games() {
+        return userLocalDatabase.getBoolean("won10Games", false);
+    }
+
+    public boolean getWon20Games() {
+        return userLocalDatabase.getBoolean("won20Games", false);
+    }
+
+    public boolean getWon50Games() {
+        return userLocalDatabase.getBoolean("won50Games", false);
+    }
+
+    public boolean getWon100Games() {
+        return userLocalDatabase.getBoolean("won100Games", false);
+    }
+
+    public boolean getWon250Games() {
+        return userLocalDatabase.getBoolean("won250Games", false);
+    }
+
+    public boolean getWonInExactly69() {
+        return userLocalDatabase.getBoolean("wonInExactly69", false);
+    }
+
+    public boolean getWonInExactly100() {
+        return userLocalDatabase.getBoolean("wonInExactly100", false);
+    }
+
+    public boolean getWonInExactly420() {
+        return userLocalDatabase.getBoolean("wonInExactly420", false);
+    }
+
+    public boolean getWonInOver500() {
+        return userLocalDatabase.getBoolean("wonInOver500", false);
+    }
+
+    public boolean getWonInOver700() {
+        return userLocalDatabase.getBoolean("wonInOver700", false);
+    }
+
+    public boolean getWonInOver800() {
+        return userLocalDatabase.getBoolean("wonInOver800", false);
+    }
+
+    public boolean getWonInOver900() {
+        return userLocalDatabase.getBoolean("wonInOver900", false);
+    }
+
+    public boolean getWonInOver1000() {
+        return userLocalDatabase.getBoolean("wonInOver1000", false);
+    }
+
+    public boolean getWonInOver9000() {
+        return userLocalDatabase.getBoolean("wonInOver9000", false);
+    }
+
+
+}
